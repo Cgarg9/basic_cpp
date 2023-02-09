585a6548efd049b5f56783b8159ec6afd16046d0
Cgarg9 <95637160+Cgarg9@users.noreply.github.com>
2023-02-09 20:40:52 +0530

    Create matrix_multiplication.cpp

---
+++ matrix_multiplication.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n1, n2, n3 = 0;
+    cin >> n1 >> n2 >> n3;
+    int m1[n1][n2], m2[n2][n3];
+    for (int i = 0; i < n1; i++)
+    {
+        for (int j = 0; j < n2; j++)
+        {
+            cin >> m1[i][j];
+        }
+    }
+
+    for (int i = 0; i < n2; i++)
+    {
+        for (int j = 0; j < n3; j++)
+        {
+            cin >> m2[i][j];
+        }
+    }
+
+    int m3[n1][n3];
+//  initialising answer matrix with 0
+    for (int i = 0; i < n1; i++)
+    {
+        for (int j = 0; j < n3; j++)
+        {
+            m3[i][j] = 0;
+        }
+    }
+    for (int i = 0; i < n1; i++)
+    {
+        for (int j = 0; j < n3; j++)
+        {
+            cout << m3[i][j] << " ";
+        }
+        cout << "\n";
+    }
+//  printing answer initial answer matrix
+    for (int i = 0; i < n1; i++)
+    {
+        for (int j = 0; j < n3; j++)
+        {
+            for (int k = 0; k < n2; k++)
+            {
+                m3[i][j] += m1[i][k] * m2[k][j];
+            }
+        }
+    }
+    for (int i = 0; i < n1; i++)
+    {
+        for (int j = 0; j < n3; j++)
+        {
+            cout << m3[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
