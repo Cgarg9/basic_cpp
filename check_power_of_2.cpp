4f5b14d32b2b826e2fda2c5b1b4fd8da177c96ef
Cgarg9 <95637160+Cgarg9@users.noreply.github.com>
2023-02-11 13:39:01 +0530

    Create check_power_of_2.cpp

---
+++ check_power_of_2.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool ispower_2(int n)
+{
+    // 8 = 1000;
+    // 8-1 = 7 = 0111;
+    // 7 & 8 = 0
+    return ((n & (n - 1)) == 0);
+}
+int main()
+{
+    int n;
+    cin >> n;
+    if (ispower_2(n))
+    {
+        cout << n << " is a power of 2\n";
+    }
+    else
+    {
+        cout << n << " is not a power of 2\n";
+    }
+    return 0;
+}
